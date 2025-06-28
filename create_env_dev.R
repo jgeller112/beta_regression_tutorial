cd23f817c5e8cf42642028ba7078d591f47d1cc8
jgeller <gellerja@bc.edu>
2025-06-28 11:57:13 -0400

    positron push ms

--- create_env_dev.R
+++ create_env_dev.R
@@ -1,31 +1,78 @@
 #need this to set path to nix for some reason
 # Sys.setenv(PATH = paste("/nix/var/nix/profiles/default/bin", Sys.getenv("PATH"), sep=":"))
 
-
-required_packages  = c("tidyverse", "brms", "quarto", "tinytable", "marginaleffects", "extraDistr","easystats",
-                       "scales", "tidybayes", "webshot2", "ggbeeswarm", "gghalves", 
-                       "here", "posterior", "ggokabeito",
-                       "patchwork", "cowplot", "viridis", "collapse", "transformr", 
-                       "ggrain", "glmmTMB")
+required_packages = c(
+  "tidyverse",
+  "brms",
+  "quarto",
+  "tinytable",
+  "marginaleffects",
+  "extraDistr",
+  "easystats",
+  "scales",
+  "tidybayes",
+  "webshot2",
+  "ggbeeswarm",
+  "gghalves",
+  "here",
+  "posterior",
+  "ggokabeito",
+  "patchwork",
+  "cowplot",
+  "viridis",
+  "collapse",
+  "transformr",
+  "ggrain",
+  "glmmTMB"
+)
 
 library(rix)
 
-rix(date = "2025-04-07",
-    r_pkgs = required_packages, 
-    system_pkgs = c("quarto", "git", "pandoc", "typst", "stanc", "tbb"),
-    git_pkgs = list(
-      list(
-        package_name = "cmdstanr",
-        repo_url = "https://github.com/stan-dev/cmdstanr",
-        commit = "541f36c74c236a322eaa0908e2e86425790ca2cf")
-      , list(
-        package_name = "ordbetareg_pack",
-        repo_url = "https://github.com/saudiwin/ordbetareg_pack", 
-        commit = "673292ba8f1b0b0978af8732579f42fc0b37c6ff"
-      )), 
-    tex_pkgs = c("amsmath", "ninecolors", "apa7", "scalerel", "threeparttable", "threeparttablex", "endfloat", "environ", "multirow", "tcolorbox", "pdfcol", "tikzfill", "fontawesome5", "framed", "newtx", "fontaxes", "xstring", "wrapfig", "tabularray", "siunitx", 
-                 "fvextra", "geometry","setspace", "fancyvrb", "anyfontsize"), 
-    shell_hook = "Rscript install_cmdstan.R",
-    ide = "code",
-    project_path = ".",
-    overwrite = TRUE)
+rix(
+  date = "2025-04-07",
+  r_pkgs = required_packages,
+  system_pkgs = c("quarto", "git", "pandoc", "typst", "stanc", "tbb"),
+  git_pkgs = list(
+    list(
+      package_name = "cmdstanr",
+      repo_url = "https://github.com/stan-dev/cmdstanr",
+      commit = "541f36c74c236a322eaa0908e2e86425790ca2cf"
+    ),
+    list(
+      package_name = "ordbetareg_pack",
+      repo_url = "https://github.com/saudiwin/ordbetareg_pack",
+      commit = "673292ba8f1b0b0978af8732579f42fc0b37c6ff"
+    )
+  ),
+  tex_pkgs = c(
+    "amsmath",
+    "ninecolors",
+    "apa7",
+    "scalerel",
+    "threeparttable",
+    "threeparttablex",
+    "endfloat",
+    "environ",
+    "multirow",
+    "tcolorbox",
+    "pdfcol",
+    "tikzfill",
+    "fontawesome5",
+    "framed",
+    "newtx",
+    "fontaxes",
+    "xstring",
+    "wrapfig",
+    "tabularray",
+    "siunitx",
+    "fvextra",
+    "geometry",
+    "setspace",
+    "fancyvrb",
+    "anyfontsize"
+  ),
+  shell_hook = "Rscript install_cmdstan.R",
+  ide = "code",
+  project_path = ".",
+  overwrite = TRUE
+)
